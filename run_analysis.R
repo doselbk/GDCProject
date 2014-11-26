21a62cbe0ae858b3649616253860735857a177a4
doselkim <bkim@Joe.home>
2014-11-26 11:27:06 -0500

    update

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,30 @@
 # Code
 
+packages.check <- function(require.packages) {
+# PARAMETER: take a list of package names 
+# check required packages from installed.packages() 
+# - if found, load the package
+# - if not found, install and load the package
+  for (i in require.packages)
+    if (i %in% rownames(installed.packages()) == TRUE) {
+      library(i, character.only = TRUE)
+    } else {
+      install.packages(i, character.only = TRUE)
+      library(i, character.only = TRUE)
+    }
+}
+
+
+
+# the data file source
 data.url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
-
 # Check for the required packages
-# - if not found, install the packages
+# - if not found, install the missing package
+# - load the required packages
 
-# Load the required packages
+require.packages <- c("plyr", "reshape2")
+packages.check(require.packages)
 
 # Set the current directory as the working directory
 this.dir <- dirname(parent.frame(2)$ofile)
@@ -39,9 +57,7 @@ if (!file.exists(data.file)) {
   cat("- found", data.file, "\n")
 }
 
-data.unzipped.dir <- paste(data.dir, "UCI HAR Dataset", sep = "/", collapse = "/")
-
-# At this point, the data files should be in /data/UCI HAR Dataset/
+# At this point, the data files should be in ./data/UCI HAR Dataset/
 # 
 # Data to load:
 # /activity_labels.txt | complete list of activities
@@ -55,14 +71,27 @@ data.unzipped.dir <- paste(data.dir, "UCI HAR Dataset", sep = "/", collapse = "/
 # /train/y_train | activity list
 # /train/X_train | train data file
 
-activity.label <- read.table("./data/UCI HAR Dataset/activity_labels.txt", header=FALSE, col.names = c("ID", "Activity"))
+activity.label <- read.table("./data/UCI HAR Dataset/activity_labels.txt", header=FALSE, col.names = c("ActivityID", "ActivityType"))
 features.label <- read.table("./data/UCI HAR Dataset/features.txt", header=FALSE, col.names = c("ID", "Feature"))
 
-subject.test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt", header=FALSE, col.names = c("Subject.ID"))
-activity.test <- read.table("./data/UCI HAR Dataset/test/y_test.txt", header=FALSE, col.names = c("Activity.ID"))
+cat("building the test data\n")
+subject.test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt", header=FALSE, col.names = c("Subject"))
+activity.test <- read.table("./data/UCI HAR Dataset/test/y_test.txt", header=FALSE, col.names = c("ActivityID"))
+activity.test <- join(activity.test, activity.label, by="ActivityID")
 data.test <- read.table("./data/UCI HAR Dataset/test/X_test.txt", header=FALSE, col.names = features.label$Feature)
+data.test <- cbind(subject.test, activity.test, data.test)
+data.test <- mutate(data.test, datasource = "Test")
+cat("done building the test data\n")
 
-subject.train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt", header=FALSE, col.names = ("Subject.ID"))
-activity.train <- read.table("./data/UCI HAR Dataset/train/y_train.txt", header=FALSE, col.names = c("Activity.ID"))
+cat("building the train data\n")
+subject.train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt", header=FALSE, col.names = ("Subject"))
+activity.train <- read.table("./data/UCI HAR Dataset/train/y_train.txt", header=FALSE, col.names = c("ActivityID"))
+activity.train <- join(activity.train, activity.label, by="ActivityID")
 data.train <- read.table("./data/UCI HAR Dataset/train/X_train.txt", header=FALSE, col.names = features.label$Feature)
+data.train <- cbind(subject.train, activity.train, data.train)
+data.train <- mutate(data.train, datasource = "Train")
+cat("done building the train data\n")
 
+cat("merge the test and train data into one\n")
+dt <- rbind(data.test, data.train)
+cat("done merging\n")
